090fbd8a0d07902f5c367389e8853fe631b48376
ajlopez <webmaster@ajlopez.com>
2011-12-03 16:48:00 -0300

    Workspace, with first test

---
+++ Src/AjCoRe.Tests/WorkspaceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AjCoRe.Tests
+{
+    [TestClass]
+    public class WorkspaceTests
+    {
+        [TestMethod]
+        public void CreateWorkspaceWithRootNode()
+        {
+            Node root = new Node(null);
+            Workspace workspace = new Workspace("repository", root);
+
+            Assert.AreEqual(root, workspace.RootNode);
+            Assert.AreEqual("repository", workspace.Name);
+        }
+    }
+}
---
+++ Src/AjCoRe/Workspace.cs
@@ -0,0 +1,23 @@
+﻿namespace AjCoRe
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Workspace
+    {
+        private string name;
+        private Node root;
+
+        public Workspace(string name, Node root)
+        {
+            this.name = name;
+            this.root = root;
+        }
+
+        public string Name { get { return this.name; } }
+
+        public Node RootNode { get { return this.root; } }
+    }
+}
