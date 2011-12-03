e143f4e0daacaf5341f8374f4763fe48d3c01b57
ajlopez <webmaster@ajlopez.com>
2011-12-03 11:26:41 -0300

    Initial solution; Property with first test

---
+++ Src/AjCoRe.Tests/PropertyTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AjCoRe.Tests
+{
+    [TestClass]
+    public class PropertyTests
+    {
+        [TestMethod]
+        public void CreateProperty()
+        {
+            Property prop = new Property("Name", "John");
+
+            Assert.AreEqual("Name", prop.Name);
+            Assert.AreEqual("John", prop.Value);
+        }
+    }
+}
---
+++ Src/AjCoRe/Property.cs
@@ -0,0 +1,23 @@
+﻿namespace AjCoRe
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Property
+    {
+        private string name;
+        private object value;
+
+        public string Name { get { return this.name; } }
+
+        public object Value { get { return this.value; } }
+
+        public Property(string name, object value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+    }
+}
