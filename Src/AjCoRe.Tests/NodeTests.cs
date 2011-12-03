c11bd009a17e112c142db4769b457c0b39110555
ajlopez <webmaster@ajlopez.com>
2011-12-03 11:40:42 -0300

    Node, first test

---
+++ Src/AjCoRe.Tests/NodeTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AjCoRe.Tests
+{
+    [TestClass]
+    public class NodeTests
+    {
+        public void CreateNode()
+        {
+            Node node = new Node("person", new List<Property>()
+            {
+                new Property("Name", "Adam"),
+                new Property("Age", 800)
+            });
+
+            Assert.IsNotNull(node.Properties);
+            Assert.IsNotNull(node.Properties.Where(p => p.Name == "Name").SingleOrDefault());
+            Assert.AreEqual("Adam", node.Properties.Where(p => p.Name == "Name").SingleOrDefault().Value);
+            Assert.IsNotNull(node.Properties.Where(p => p.Name == "Age").SingleOrDefault());
+            Assert.AreEqual(800, node.Properties.Where(p => p.Name == "Age").SingleOrDefault().Value);
+        }
+    }
+}
---
+++ Src/AjCoRe/Node.cs
@@ -0,0 +1,23 @@
+﻿namespace AjCoRe
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Node
+    {
+        string id;
+        string name;
+        IList<Property> properties;
+
+        public IEnumerable<Property> Properties { get { return this.properties; } }
+
+        public Node(string name, IEnumerable<Property> properties)
+        {
+            this.name = name;
+
+            this.properties = new List<Property>(properties);
+        }
+    }
+}
