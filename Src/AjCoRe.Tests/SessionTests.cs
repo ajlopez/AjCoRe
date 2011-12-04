32db6d2bf2c3b97059078f4e41f507c4e4a379f3
ajlopez <webmaster@ajlopez.com>
2011-12-04 11:40:33 -0300

    Session, first test

---
+++ Src/AjCoRe.Tests/SessionTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AjCoRe.Base;
+
+namespace AjCoRe.Tests
+{
+    [TestClass]
+    public class SessionTests
+    {
+        private IWorkspace workspace;
+        private WorkspaceRegistry registry;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.workspace = new Workspace("ws1", new Node(null));
+            this.registry = new WorkspaceRegistry();
+            this.registry.RegisterWorkspace(this.workspace);
+        }
+
+        [TestMethod]
+        public void CreateSession()
+        {
+            Session session = new Session(this.workspace);
+
+            Assert.IsNotNull(session.Workspace);
+            Assert.AreEqual(this.workspace, session.Workspace);
+        }
+    }
+}
+
+
---
+++ Src/AjCoRe/Session.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AjCoRe
+{
+    public class Session
+    {
+        private IWorkspace workspace;
+
+        public Session(IWorkspace workspace)
+        {
+            this.workspace = workspace;
+        }
+
+        public IWorkspace Workspace { get { return this.workspace; } }
+    }
+}
+
