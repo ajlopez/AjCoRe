a0ffc4f2f4c611ef7f1775c4d6c3eda5cb21ef88
ajlopez <webmaster@ajlopez.com>
2011-12-04 11:28:13 -0300

    WorkspaceRegistry, first test

---
+++ Src/AjCoRe.Tests/WorkspaceRegistryTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AjCoRe.Base;
+
+namespace AjCoRe.Tests
+{
+    [TestClass]
+    public class WorkspaceRegistryTests
+    {
+        [TestMethod]
+        public void RegisterAndRetrieveWorkspace()
+        {
+            WorkspaceRegistry registry = new WorkspaceRegistry();
+            INode root = new Node(null);
+            IWorkspace workspace = new Workspace("ws1", root);
+            registry.RegisterWorkspace(workspace);
+
+            Assert.IsNotNull(registry.Workspaces);
+            Assert.AreEqual(1, registry.Workspaces.Count());
+
+            var ws = registry["ws1"];
+            Assert.IsNotNull(ws);
+            Assert.AreEqual("ws1", ws.Name);
+            Assert.AreEqual(workspace, ws);
+
+            Assert.IsNull(registry["ws2"]);
+        }
+    }
+}
---
+++ Src/AjCoRe/WorkspaceRegistry.cs
@@ -0,0 +1,27 @@
+﻿namespace AjCoRe
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class WorkspaceRegistry
+    {
+        private IList<IWorkspace> workspaces = new List<IWorkspace>();
+
+        public IEnumerable<IWorkspace> Workspaces { get { return this.workspaces; } }
+
+        public IWorkspace this[string name]
+        {
+            get
+            {
+                return this.workspaces.Where(ws => ws.Name == name).SingleOrDefault();
+            }
+        }
+
+        public void RegisterWorkspace(IWorkspace workspace)
+        {
+            workspaces.Add(workspace);
+        }
+    }
+}
