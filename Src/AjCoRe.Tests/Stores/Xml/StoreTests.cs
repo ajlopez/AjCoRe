0d3500bc4047ed39b8334b239a44d15532d4bc6d
ajlopez <webmaster@ajlopez.com>
2011-12-11 15:53:32 -0300

    Xml Store manage simple types, with tests

--- Src/AjCoRe.Tests/Stores/Xml/StoreTests.cs
+++ Src/AjCoRe.Tests/Stores/Xml/StoreTests.cs
@@ -34,17 +34,48 @@ namespace AjCoRe.Tests.Stores.Xml
         {
             PropertyList properties = new PropertyList(new List<Property>()
                 {
-                    new Property("Name", "Adam"),
-                    new Property("Age", 800)
+                    new Property("Name", "Abel"),
+                    new Property("Age", 400)
                 });
 
             Store store = new Store("xmlfs");
 
-            store.SaveProperties("/adam", properties);
+            store.SaveProperties("/abel", properties);
 
-            var props = store.LoadProperties("/adam");
+            var props = store.LoadProperties("/abel");
 
             Assert.AreEqual(2, props.Count());
+            Assert.AreEqual("Abel", props["Name"].Value);
+            Assert.AreEqual(400, props["Age"].Value);
+        }
+
+        [TestMethod]
+        [DeploymentItem("Files/XmlFileSystem", "xmlfs")]
+        public void SaveAndPropertiesWithSimpleTypes()
+        {
+            PropertyList properties = new PropertyList(new List<Property>()
+                {
+                    new Property("Name", "Eve"),
+                    new Property("Age", 600),
+                    new Property("Male", false),
+                    new Property("Hired", new DateTime(2000, 1, 1)),
+                    new Property("Height", (double) 10.2),
+                    new Property("Salary", (decimal) 200.50)
+                });
+
+            Store store = new Store("xmlfs");
+
+            store.SaveProperties("/eve", properties);
+
+            var props = store.LoadProperties("/eve");
+
+            Assert.AreEqual(6, props.Count());
+            Assert.AreEqual("Eve", props["Name"].Value);
+            Assert.AreEqual(600, props["Age"].Value);
+            Assert.AreEqual(false, props["Male"].Value);
+            Assert.AreEqual(new DateTime(2000, 1, 1), props["Hired"].Value);
+            Assert.AreEqual((double)10.2, props["Height"].Value);
+            Assert.AreEqual((decimal)200.50, props["Salary"].Value);
         }
     }
 }
--- Src/AjCoRe/Stores/Xml/Store.cs
+++ Src/AjCoRe/Stores/Xml/Store.cs
@@ -19,13 +19,27 @@
         public void SaveProperties(string path, PropertyList properties)
         {
             string filename = GetFileNameFromPath(path);
-            XmlWriter writer = XmlWriter.Create(filename);
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            XmlWriter writer = XmlWriter.Create(filename, settings);
             writer.WriteStartDocument();
             writer.WriteStartElement("Properties");
 
             foreach (Property property in properties)
             {
                 writer.WriteStartElement(property.Name);
+
+                if (property.Value is int)
+                    writer.WriteAttributeString("type", "int");
+                else if (property.Value is DateTime)
+                    writer.WriteAttributeString("type", "datetime");
+                else if (property.Value is decimal)
+                    writer.WriteAttributeString("type", "decimal");
+                else if (property.Value is double)
+                    writer.WriteAttributeString("type", "double");
+                else if (property.Value is bool)
+                    writer.WriteAttributeString("type", "bool");
+
                 writer.WriteValue(property.Value);
                 writer.WriteEndElement();
             }
@@ -47,6 +61,7 @@
 
             string name = null;
             object value = null;
+            string type = null;
 
             while (reader.Read())
             {
@@ -54,18 +69,42 @@
                 {
                     case XmlNodeType.Element:
                         if (!started && reader.Name == "Properties")
+                        {
                             started = true;
-                        else
-                            name = reader.Name;
+                            continue;
+                        }
+
+                        name = reader.Name;
+
+                        if (reader.HasAttributes && reader.MoveToAttribute("type"))
+                            type = reader.Value;
+
                         break;
 
                     case XmlNodeType.Text:
-                        value = reader.Value;
+                        if (!started)
+                            continue;
+
+                        if (type == "int")
+                            value = XmlConvert.ToInt32(reader.Value);
+                        else if (type == "datetime")
+                            value = XmlConvert.ToDateTime(reader.Value, XmlDateTimeSerializationMode.Unspecified);
+                        else if (type == "bool")
+                            value = XmlConvert.ToBoolean(reader.Value);
+                        else if (type == "decimal")
+                            value = XmlConvert.ToDecimal(reader.Value);
+                        else if (type == "double")
+                            value = XmlConvert.ToDouble(reader.Value);
+                        else
+                            value = reader.Value;
+
                         if (started && name != null)
                         {
                             properties.Add(new Property(name, value));
                             name = null;
+                            type = null;
                         }
+
                         break;
 
                     default:
