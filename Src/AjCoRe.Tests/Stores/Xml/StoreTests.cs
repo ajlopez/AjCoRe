0b5af011c1c945825f5275b8626636be4673e0cf
ajlopez <webmaster@ajlopez.com>
2011-12-11 14:59:29 -0300

    Store, Xml, first tests

---
+++ Src/AjCoRe.Tests/Stores/Xml/StoreTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AjCoRe.Stores.Xml;
+using System.IO;
+
+namespace AjCoRe.Tests.Stores.Xml
+{
+    [TestClass]
+    public class StoreTests
+    {
+        [TestMethod]
+        [DeploymentItem("Files/XmlFileSystem", "xmlfs")]
+        public void SaveSimpleProperties()
+        {
+            PropertyList properties = new PropertyList(new List<Property>()
+                {
+                    new Property("Name", "Adam"),
+                    new Property("Age", 800)
+                });
+
+            Store store = new Store("xmlfs");
+
+            store.SaveProperties("/adam", properties);
+
+            Assert.IsTrue(File.Exists("xmlfs/adam.xml"));
+        }
+
+        [TestMethod]
+        [DeploymentItem("Files/XmlFileSystem", "xmlfs")]
+        public void SaveAndSimpleProperties()
+        {
+            PropertyList properties = new PropertyList(new List<Property>()
+                {
+                    new Property("Name", "Adam"),
+                    new Property("Age", 800)
+                });
+
+            Store store = new Store("xmlfs");
+
+            store.SaveProperties("/adam", properties);
+
+            var props = store.LoadProperties("/adam");
+
+            Assert.AreEqual(2, props.Count());
+        }
+    }
+}
---
+++ Src/AjCoRe/Stores/Xml/Store.cs
@@ -0,0 +1,84 @@
+﻿namespace AjCoRe.Stores.Xml
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Xml;
+    using System.IO;
+
+    public class Store
+    {
+        private string rootpath;
+
+        public Store(string rootpath)
+        {
+            this.rootpath = rootpath;
+        }
+
+        public void SaveProperties(string path, PropertyList properties)
+        {
+            string filename = GetFileNameFromPath(path);
+            XmlWriter writer = XmlWriter.Create(filename);
+            writer.WriteStartDocument();
+            writer.WriteStartElement("Properties");
+
+            foreach (Property property in properties)
+            {
+                writer.WriteStartElement(property.Name);
+                writer.WriteValue(property.Value);
+                writer.WriteEndElement();
+            }
+
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+
+            writer.Close();
+        }
+
+        public PropertyList LoadProperties(string path)
+        {
+            string filename = GetFileNameFromPath(path);
+            XmlReader reader = XmlReader.Create(filename);
+
+            IList<Property> properties = new List<Property>();
+
+            bool started = false;
+
+            string name = null;
+            object value = null;
+
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        if (!started && reader.Name == "Properties")
+                            started = true;
+                        else
+                            name = reader.Name;
+                        break;
+
+                    case XmlNodeType.Text:
+                        value = reader.Value;
+                        if (started && name != null)
+                        {
+                            properties.Add(new Property(name, value));
+                            name = null;
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            return new PropertyList(properties);
+        }
+
+        private string GetFileNameFromPath(string path)
+        {
+            return Path.Combine(rootpath, path.Substring(1)) + ".xml";
+        }
+    }
+}
